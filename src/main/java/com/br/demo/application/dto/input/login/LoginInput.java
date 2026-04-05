67295a06698cf4a32555340a4055535c7ed5bebc
Antonio Romes <antonioromes1@hotmail.com>
2026-04-05 10:29:41 -0300

    adiciona validação no dtos de entrada.

--- src/main/java/com/br/demo/application/dto/input/login/LoginInput.java
+++ src/main/java/com/br/demo/application/dto/input/login/LoginInput.java
@@ -1,7 +1,12 @@
 package com.br.demo.application.dto.input.login;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record LoginInput(
-        String email,
-        String password) {
+                @Email(message = "Email no formato inválido") @NotBlank(message = "Email é obrigatório") String email,
+
+                @NotBlank(message = "Senha é obrigatória") @Size(min = 8, message = "A senha deve conter no mínimo 8 caracteres") String password) {
 
 }
--- src/main/java/com/br/demo/application/dto/input/user/RegisterUserInput.java
+++ src/main/java/com/br/demo/application/dto/input/user/RegisterUserInput.java
@@ -1,8 +1,12 @@
 package com.br.demo.application.dto.input.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record RegisterUserInput(
-        String nomeCompleto,
-        String nickname,
-        String email,
-        String password) {
+                @NotBlank(message = "Nome completo é obrigatório") String nomeCompleto,
+                @NotBlank(message = "Apelido é obrigatório") String nickname,
+                @NotBlank(message = "Email é obrigatório") @Email(message = "Email no formato inválido") String email,
+                @NotBlank(message = "Senha é obrigatória") @Size(min = 8, message = "A senha deve conter no mínimo 8 caracteres") String password) {
 }
